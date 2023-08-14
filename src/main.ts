562c5e8c73291c6dfdc063db53b50f058cad4238
soumen-pradhan <sam.thepradhans@gmail.com>
2023-08-14 22:07:21 +0530

    show error when no webgpu

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { throwError, log, sleep } from './utils';
+import { throwError, log, sleep, writeCanvas } from './utils';
 
 import vertShaderCode from './shaders/tri.vert.wgsl?raw';
 import fragShaderCode from './shaders/tri.frag.wgsl?raw';
@@ -17,18 +17,9 @@ import {
     vertices,
 } from './meshes/rect';
 
-import {
-    samplerDesc,
-    textureBindGroupLayoutDesc,
-    textureDesc,
-} from './texture';
-
 import { roms, keys, beep } from './info';
 
-/** Check and set up device */
-
-const canvas: HTMLCanvasElement =
-    document.querySelector('#chip-display') ?? throwError('No canvas element');
+const canvas: HTMLCanvasElement = document.querySelector('#chip-display')!;
 {
     // Set width and height of canvas according to 2:1
     const aspectRatio = 2;
@@ -39,225 +30,24 @@ const canvas: HTMLCanvasElement =
     log(`canvas: ${canvas.width} x ${canvas.height}`);
 }
 
-const gpu = navigator.gpu ?? throwError('WebGPU is not supported');
-const device =
-    (await (await gpu.requestAdapter())?.requestDevice()) ??
-    throwError('No device found');
+/** Create Chip and load default ROM */
 
-const canvasFormat = gpu.getPreferredCanvasFormat();
-
-const context =
-    canvas.getContext('webgpu') ??
-    throwError('No webgpu context for the canvas');
-{
-    context.configure({
-        device,
-        format: canvasFormat,
-        usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
-        alphaMode: 'opaque',
-    });
-}
-
-const depthTexture = device.createTexture({
-    size: [canvas.width, canvas.height, 1],
-    dimension: '2d',
-    format: 'depth24plus-stencil8',
-    usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
-});
-
-/** The Rectangle buffers and uniform */
-
-function createBuffer(arr: Float32Array | Uint16Array, usage: number) {
-    const buffer = device.createBuffer({
-        size: (arr.byteLength + 3) & ~3, // for alignment ??
-        usage,
-        mappedAtCreation: true,
-    });
-
-    const writeArray =
-        arr instanceof Float32Array
-            ? new Float32Array(buffer.getMappedRange())
-            : new Uint16Array(buffer.getMappedRange());
-
-    writeArray.set(arr);
-    buffer.unmap();
-
-    return buffer;
-}
-
-const vertexBuffer = createBuffer(vertices, GPUBufferUsage.VERTEX);
-const colorBuffer = createBuffer(colors, GPUBufferUsage.VERTEX);
-const indexBuffer = createBuffer(indices, GPUBufferUsage.INDEX);
-
-const uniformBuffer = createBuffer(
-    uniformData,
-    GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-);
-const uniformBindGroupLayout: GPUBindGroupLayout = device.createBindGroupLayout(
-    {
-        entries: [
-            {
-                binding: 0,
-                visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
-                buffer: {},
-            },
-        ],
-    },
-);
-const uniformBindGroup = device.createBindGroup({
-    layout: uniformBindGroupLayout,
-    entries: [
-        {
-            binding: 0,
-            resource: { buffer: uniformBuffer },
-        },
-    ],
-});
-
-/** Map memory and create the texture, sampler  */
-
-const data = await fetch(roms.get('IBM Logo')!);
+const data = await fetch(roms.get('Chip8 Pic')!);
 const array = await data.arrayBuffer();
 const buff = new Uint8Array(array);
 
 const chip = Chip.new();
 chip.load(buff);
 
-const [chip_wt, chip_ht] = [Chip.vram_width(), Chip.vram_height()];
-
-// live reload, since it's a reference
-const textureData = new Uint8Array(
-    memory.buffer,
-    chip.vram_ptr(),
-    chip_wt * chip_ht,
-);
-
-const texture = device.createTexture(textureDesc);
-const sampler = device.createSampler(samplerDesc);
-
-const textureBindGroupLayout = device.createBindGroupLayout(
-    textureBindGroupLayoutDesc,
-);
-const textureBindGroup = device.createBindGroup({
-    layout: textureBindGroupLayout,
-    entries: [
-        {
-            binding: 1,
-            resource: sampler,
-        },
-        {
-            binding: 2,
-            resource: texture.createView(),
-        },
-    ],
-});
-
-/** The GPU Pipeline */
-
-const pipeline = device.createRenderPipeline({
-    layout: device.createPipelineLayout({
-        bindGroupLayouts: [uniformBindGroupLayout, textureBindGroupLayout], // @group(0), @group(1)
-    }),
-
-    vertex: {
-        module: device.createShaderModule({ code: vertShaderCode }),
-        entryPoint: 'main',
-        buffers: [vertexBufferDesc, colorBufferDesc],
-    },
-
-    fragment: {
-        module: device.createShaderModule({ code: fragShaderCode }),
-        entryPoint: 'main',
-        targets: [{ format: canvasFormat }],
-    },
-
-    primitive: {
-        cullMode: 'none',
-        frontFace: 'cw',
-        topology: 'triangle-list',
-    },
-
-    depthStencil: {
-        depthWriteEnabled: true,
-        depthCompare: 'less',
-        format: 'depth24plus-stencil8',
-    },
-});
-
-/** Render loop */
-
-async function render(_now: DOMHighResTimeStamp) {
-    while (chip.next()) {
-        const output = chip.tick();
-        if (output.beep) beep.play();
-        if (output.vram) break;
-        await sleep(0.05);
-    }
-
-    device.queue.writeTexture(
-        { texture },
-        textureData,
-        { offset: 0, bytesPerRow: chip_wt },
-        { width: chip_wt, height: chip_ht },
-    );
-
-    const colorTextureView = context.getCurrentTexture().createView();
-
-    const colorAttachment: GPURenderPassColorAttachment = {
-        view: colorTextureView,
-        clearValue: { r: 0, g: 0, b: 0, a: 1 },
-        loadOp: 'clear',
-        storeOp: 'store',
-    };
-
-    const cmdEncoder = device.createCommandEncoder();
-    const passEncoder = cmdEncoder.beginRenderPass({
-        colorAttachments: [colorAttachment],
-        depthStencilAttachment: {
-            view: depthTexture.createView(),
-            depthClearValue: 1,
-            depthLoadOp: 'clear',
-            depthStoreOp: 'store',
-            stencilClearValue: 0,
-            stencilLoadOp: 'clear',
-            stencilStoreOp: 'store',
-        },
-    });
-
-    {
-        passEncoder.setPipeline(pipeline);
-        passEncoder.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
-        passEncoder.setScissorRect(0, 0, canvas.width, canvas.height);
-
-        // pipeline.layout.bindGroupLayouts
-        passEncoder.setBindGroup(0, uniformBindGroup);
-        passEncoder.setBindGroup(1, textureBindGroup);
-
-        // pipeline.vertex.buffers
-        passEncoder.setVertexBuffer(0, vertexBuffer);
-        passEncoder.setVertexBuffer(1, colorBuffer);
-
-        passEncoder.setIndexBuffer(indexBuffer, 'uint16');
-        passEncoder.drawIndexed(6);
-    }
-
-    passEncoder.end();
-    device.queue.submit([cmdEncoder.finish()]);
-
-    requestAnimationFrame(render);
-}
-
 /** Interactivity */
 
 // Possible Bug: Race condition for chip access
 
 const keyOg: NodeListOf<HTMLElement> =
-    document.querySelectorAll('#key-og span.key') ??
-    throwError('No Chip8 Keys Table');
+    document.querySelectorAll('#key-og span.key')!;
 
 const keyMap: NodeListOf<HTMLElement> =
-    document.querySelectorAll('#key-map span.key') ??
-    throwError('No Mapped Keys Table');
+    document.querySelectorAll('#key-map span.key')!;
 
 function addKeyHandlers(chip: Chip) {
     document.addEventListener('keydown', (event) => {
@@ -287,11 +77,10 @@ addKeyHandlers(chip);
 
 /** DropDown Menu (from info.ts) */
 
-const selectRom: HTMLSelectElement =
-    document.querySelector('#roms') ?? throwError('No ROM selection menu');
+const selectRom: HTMLSelectElement = document.querySelector('#roms')!;
 
 selectRom.addEventListener('change', async (_event) => {
-    const rom = roms.get(selectRom.value) ?? 'roms/loktar00/IBM Logo.ch8';
+    const rom = roms.get(selectRom.value)!;
 
     const data = await (await fetch(rom)).arrayBuffer();
     const buff = new Uint8Array(data);
@@ -307,5 +96,239 @@ for (let [value, _] of roms) {
     selectRom.appendChild(opt);
 }
 
-/** Start main loop */
-requestAnimationFrame(render);
+/** Render Logic */
+
+try {
+    /** Check and set up device */
+    const gpu = navigator.gpu ?? throwError('WebGPU is not supported');
+    const device =
+        (await (await gpu.requestAdapter())?.requestDevice()) ??
+        throwError('No device found');
+
+    const canvasFormat = gpu.getPreferredCanvasFormat();
+
+    const context =
+        canvas.getContext('webgpu') ??
+        throwError('No webgpu context for the canvas');
+    {
+        context.configure({
+            device,
+            format: canvasFormat,
+            usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+            alphaMode: 'opaque',
+        });
+    }
+
+    const depthTexture = device.createTexture({
+        size: [canvas.width, canvas.height, 1],
+        dimension: '2d',
+        format: 'depth24plus-stencil8',
+        usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC,
+    });
+
+    /** The Rectangle buffers and uniform */
+
+    function createBuffer(arr: Float32Array | Uint16Array, usage: number) {
+        const buffer = device.createBuffer({
+            size: (arr.byteLength + 3) & ~3, // for alignment
+            usage,
+            mappedAtCreation: true,
+        });
+
+        const writeArray =
+            arr instanceof Float32Array
+                ? new Float32Array(buffer.getMappedRange())
+                : new Uint16Array(buffer.getMappedRange());
+
+        writeArray.set(arr);
+        buffer.unmap();
+
+        return buffer;
+    }
+
+    const vertexBuffer = createBuffer(vertices, GPUBufferUsage.VERTEX);
+    const colorBuffer = createBuffer(colors, GPUBufferUsage.VERTEX);
+    const indexBuffer = createBuffer(indices, GPUBufferUsage.INDEX);
+
+    const uniformBuffer = createBuffer(
+        uniformData,
+        GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    );
+    const uniformBindGroupLayout = device.createBindGroupLayout({
+        entries: [
+            {
+                binding: 0,
+                visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                buffer: {},
+            },
+        ],
+    });
+    const uniformBindGroup = device.createBindGroup({
+        layout: uniformBindGroupLayout,
+        entries: [
+            {
+                binding: 0,
+                resource: { buffer: uniformBuffer },
+            },
+        ],
+    });
+
+    /** Map memory and create the texture, sampler  */
+
+    const [chip_wt, chip_ht] = [Chip.vram_width(), Chip.vram_height()];
+
+    // live reload, since it's a reference
+    const textureData = new Uint8Array(
+        memory.buffer,
+        chip.vram_ptr(),
+        chip_wt * chip_ht,
+    );
+
+    const texture = device.createTexture({
+        size: {
+            width: Chip.vram_width(),
+            height: Chip.vram_height(),
+            depthOrArrayLayers: 1,
+        },
+        format: 'r8unorm',
+        usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING,
+    });
+
+    const sampler = device.createSampler({
+        addressModeU: 'repeat',
+        addressModeV: 'repeat',
+        magFilter: 'nearest',
+        minFilter: 'nearest',
+    });
+
+    const textureBindGroupLayout = device.createBindGroupLayout({
+        entries: [
+            {
+                binding: 1,
+                visibility: GPUShaderStage.FRAGMENT,
+                sampler: { type: 'filtering' },
+            },
+            {
+                binding: 2,
+                visibility: GPUShaderStage.FRAGMENT,
+                texture: { sampleType: 'float' },
+            },
+        ],
+    });
+
+    const textureBindGroup = device.createBindGroup({
+        layout: textureBindGroupLayout,
+        entries: [
+            {
+                binding: 1,
+                resource: sampler,
+            },
+            {
+                binding: 2,
+                resource: texture.createView(),
+            },
+        ],
+    });
+
+    /** The GPU Pipeline */
+
+    const pipeline = device.createRenderPipeline({
+        layout: device.createPipelineLayout({
+            bindGroupLayouts: [uniformBindGroupLayout, textureBindGroupLayout], // @group(0), @group(1)
+        }),
+
+        vertex: {
+            module: device.createShaderModule({ code: vertShaderCode }),
+            entryPoint: 'main',
+            buffers: [vertexBufferDesc, colorBufferDesc],
+        },
+
+        fragment: {
+            module: device.createShaderModule({ code: fragShaderCode }),
+            entryPoint: 'main',
+            targets: [{ format: canvasFormat }],
+        },
+
+        primitive: {
+            cullMode: 'none',
+            frontFace: 'cw',
+            topology: 'triangle-list',
+        },
+
+        depthStencil: {
+            depthWriteEnabled: true,
+            depthCompare: 'less',
+            format: 'depth24plus-stencil8',
+        },
+    });
+
+    /** Render loop */
+
+    async function render(_now: DOMHighResTimeStamp) {
+        while (chip.next()) {
+            const output = chip.tick();
+            if (output.beep) beep.play();
+            if (output.vram) break;
+            await sleep(0.05);
+        }
+
+        device.queue.writeTexture(
+            { texture },
+            textureData,
+            { offset: 0, bytesPerRow: chip_wt },
+            { width: chip_wt, height: chip_ht },
+        );
+
+        const colorTextureView = context.getCurrentTexture().createView();
+
+        const colorAttachment: GPURenderPassColorAttachment = {
+            view: colorTextureView,
+            clearValue: { r: 0, g: 0, b: 0, a: 1 },
+            loadOp: 'clear',
+            storeOp: 'store',
+        };
+
+        const cmdEncoder = device.createCommandEncoder();
+        const passEncoder = cmdEncoder.beginRenderPass({
+            colorAttachments: [colorAttachment],
+            depthStencilAttachment: {
+                view: depthTexture.createView(),
+                depthClearValue: 1,
+                depthLoadOp: 'clear',
+                depthStoreOp: 'store',
+                stencilClearValue: 0,
+                stencilLoadOp: 'clear',
+                stencilStoreOp: 'store',
+            },
+        });
+
+        {
+            passEncoder.setPipeline(pipeline);
+            passEncoder.setViewport(0, 0, canvas.width, canvas.height, 0, 1);
+            passEncoder.setScissorRect(0, 0, canvas.width, canvas.height);
+
+            // pipeline.layout.bindGroupLayouts
+            passEncoder.setBindGroup(0, uniformBindGroup);
+            passEncoder.setBindGroup(1, textureBindGroup);
+
+            // pipeline.vertex.buffers
+            passEncoder.setVertexBuffer(0, vertexBuffer);
+            passEncoder.setVertexBuffer(1, colorBuffer);
+
+            passEncoder.setIndexBuffer(indexBuffer, 'uint16');
+            passEncoder.drawIndexed(6);
+        }
+
+        passEncoder.end();
+        device.queue.submit([cmdEncoder.finish()]);
+
+        requestAnimationFrame(render);
+    }
+
+    /** Start render loop */
+
+    requestAnimationFrame(render);
+} catch (e) {
+    const context = canvas.getContext('2d')!;
+    writeCanvas(context, `${e}`);
+}
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,9 @@ export const throwError = (str: string) => {
 export const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
 
 export const randu8 = (max: number = 256) => Math.floor(Math.random() * max);
+
+export const writeCanvas = (context: CanvasRenderingContext2D, msg: string) => {
+    context.font = '36px monospace';
+    context.fillStyle = 'red';
+    context.fillText(msg, 50, 50);
+};
